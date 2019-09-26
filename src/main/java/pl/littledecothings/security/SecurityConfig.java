0eb447d5a79a02fee6a1de1e161e898de1567131
Paweł Pradelok <code2pradelok@gmail.com>
2019-09-26 16:32:47 +0200

    start security

---
+++ src/main/java/pl/littledecothings/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package pl.littledecothings.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManager) throws Exception {
+        authenticationManager.inMemoryAuthentication()
+                .withUser("user1").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin1").password("{noop}admin123").roles("ADMIN");
+    }
+}
---
+++ src/main/java/pl/littledecothings/test/HomeController.java
@@ -0,0 +1,4 @@
+package pl.littledecothings.test;
+
+public class HomeController {
+}
