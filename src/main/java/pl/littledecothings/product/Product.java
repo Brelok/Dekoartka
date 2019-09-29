790b47a7fe9d2fea7804c17c09031f1d6b8456c7
Paweł Pradelok <code2pradelok@gmail.com>
2019-09-29 21:32:39 +0200

    add class product

---
+++ src/main/java/pl/littledecothings/product/Product.java
@@ -0,0 +1,30 @@
+package pl.littledecothings.product;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private boolean status;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
