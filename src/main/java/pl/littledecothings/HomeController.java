8c406c7180171db441868dbccc69fd304508b978
Paweł Pradelok <code2pradelok@gmail.com>
2019-10-02 12:13:13 +0200

    work on form

--- src/main/java/pl/littledecothings/HomeController.java
+++ src/main/java/pl/littledecothings/HomeController.java
@@ -2,10 +2,7 @@ package pl.littledecothings;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.littledecothings.product.ProductToRentService;
 
 @Controller
@@ -31,5 +28,10 @@ public class HomeController {
     @GetMapping("/contact")
     public String contact() {
         return "contact";
+    }
+
+    @PostMapping("/form")
+    public String form (){
+        return "redirect:/contact";
     }
 }
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    console.log('cvzcv');
+
+    var form = $('#button');
+
+    form.css('background', '#A27157');
+
+})
